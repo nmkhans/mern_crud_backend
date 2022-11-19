3481108ccc9726d993603d4dffab0aaf0af89e72
Moin Khan <nurmoin05@gmail.com>
2022-11-19 20:10:04 +0600

    dependency packages installed and configured

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const router = require("./src/routes/api");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
 
 //? app configuration
 const app = express();
-
+app.use(cors());
+app.use(bodyparser.json());
 
 //? database connection
+/* mongoose.connect("")
+    .then(() => console.log("database connected"))
+    .catch((error) => console.log(error)) */
 
 //? handle routes
 app.use("/api/v1", router);
 
 //? handle undefined routes
 app.use("*", (req, res) => {
-    res.status(404).json({success: false, message: "Route was not found"})
+    res.status(404).json({ success: false, message: "Route was not found" })
 })
 
 module.exports = app;
